3432b17c81ea8d0f211b19d3ac5ff1eaac813a65
Eitan Mirovski <eim224@lehigh.edu>
2023-02-13 21:46:16 -0500

    Complete Assignment 2.R

--- COVID-19_Project/Assignment 2.R
+++ COVID-19_Project/Assignment 2.R
@@ -1,1 +1,14 @@
+library(dplyr)
+US_States <- read.csv("us-states.csv")
+PENNSYLVANIA <- filter(US_States, state=="Pennsylvania") 
 
+PENNSYLVANIA$incr_cases <- 0
+PENNSYLVANIA$incr_deaths <- 0
+
+for (i in 2:length(PENNSYLVANIA$cases)) {
+PENNSYLVANIA$incr_cases[i] <- (PENNSYLVANIA$cases[i]-PENNSYLVANIA$cases[i-1]) 
+PENNSYLVANIA$incr_deaths[i] <- (PENNSYLVANIA$deaths[i]-PENNSYLVANIA$deaths[i-1])
+}
+
+std_incr_cases <- sd(PENNSYLVANIA$incr_cases)
+std_incr_cases
